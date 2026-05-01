acfa47164b636375c76c1bc159906ce736e2d5ca
hemantgupta09 <guptahmt9@gmail.com>
2026-05-01 12:56:53 +0530

    refactor: remove virtual modifier from repository methods

--- LibraryManagement.Infrastructure/Repositories/Repository.cs
+++ LibraryManagement.Infrastructure/Repositories/Repository.cs
@@ -14,8 +14,8 @@ namespace LibraryManagement.Infrastructure.Repositories
             _context = context;
             _dbSet = context.Set<T>();
         }
-        public virtual async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
-        public virtual async Task<List<T>> GetAllAsync() => await _dbSet.AsNoTracking().ToListAsync();
+        public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+        public async Task<List<T>> GetAllAsync() => await _dbSet.AsNoTracking().ToListAsync();
         public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
         public void Update(T entity) => _dbSet.Update(entity);
         public void Delete(T entity) => _dbSet.Remove(entity);
